683b05d387c5c746a30726958fcb9267c3de6c20
Ceyda Cinarel <15624271+cceyda@users.noreply.github.com>
2022-12-23 07:19:20 +0900

    add build so i can pip install

---
+++ streamlit_pills/frontend/build/precache-manifest.10258d8ee4869ef06536f265582978aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a56b541101c8f290faba3e8354915ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5fc2609b4129ef53657c",
+    "url": "./static/js/2.342ec804.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.342ec804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8727a60a9cbf8839a2",
+    "url": "./static/js/main.84ecba67.chunk.js"
+  },
+  {
+    "revision": "cdd2ffe8ba7f1494a252",
+    "url": "./static/js/runtime-main.683ff83b.js"
+  }
+]);
